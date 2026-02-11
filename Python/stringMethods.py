e114e0f344ae25c455aa35c51030436317d704ad
Reindel <94912874+Cyborg1644W@users.noreply.github.com>
2026-02-12 00:00:44 +0800

    Add files via upload
    python different sting methods

---
+++ Python/stringMethods.py
@@ -0,0 +1,19 @@
+# name = input("What is your name: ")
+number = input("What is your number: ")
+
+# result =  len(name)
+
+# result = name.find("a")
+# result = name.rfind("a") #reverse find
+# result = name.isalpha() #false if may space
+
+# result = name.isdigit()
+# name = name.capitalize()
+# name = name.title()
+# name = name.lower()
+# name = name.upper()
+
+# result = number.count()
+number = number.replace("",", ")
+
+print(number)
